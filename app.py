c140b03afd1b1b37d8ffad79c6abc6e6cc7baa79
vnatale52 <vnatale52@gmail.com>
2025-01-26 13:56:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ html_template = """
         <button type="submit">Establecer Fecha</button>
     </form>
 
-<h1>Paso 2 : Carga el archivo Tasas.xlsx</h1>
+<h1>Paso 2 : Carga el archivo Tasas.xlsx, Títulos de las 3 columnas:  F_Desde  F_Hasta_Inc. Tasa </h1>
     <form action="/upload_tasa" method="post" enctype="multipart/form-data">
         <input type="file" name="tasa_file" accept=".xlsx" required>
         <br><br>
@@ -29,7 +29,7 @@ html_template = """
     </form>
 
 
- <h1>Paso 3 : Carga el archivo Deuda.xlsx</h1>
+ <h1>Paso 3 : Carga el archivo Deuda.xlsx , Títulos de las 3 columnas: Mes y Año  Fecha_Vto  Importe_Deuda</h1>
     <form action="/process" method="post" enctype="multipart/form-data">
         <input type="file" name="excel_file" accept=".xlsx" required>
         <br><br>
